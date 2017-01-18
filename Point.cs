8dffcef2b9cb2a7891ac48a19e0602e360198ea1
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-01-18 16:56:08 -0500

    Earned the OOP badge by finishing the first stage of the course.

---
+++ Point.cs
@@ -0,0 +1,14 @@
+﻿namespace TreehouseDefense
+{
+    class Point
+    {
+        public readonly int XCoordinate;
+        public readonly int YCoordinate;
+
+        public Point(int xCoordinate, int yCoordinate)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+        }
+    }
+}
