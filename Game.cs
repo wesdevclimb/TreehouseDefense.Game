ec2b97af3e95998c0e8a67a58d1156725b06cc20
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-01-21 17:32:00 -0600

    Finished s3v2

--- Game.cs
+++ Game.cs
@@ -10,7 +10,16 @@ namespace TreehouseDefense
             Map map = new Map(8, 5);
             try
             {
-                MapLocation mapLocation = new MapLocation(8, 5, map);
+                MapLocation[] path = {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map)
+                };
             }
             catch (OutOfBoundsException ex)
             {
--- Path.cs
+++ Path.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Diagnostics;
+
 namespace TreehouseDefense
 {
     class Path
     {
-        
+        private MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
     }
 }
